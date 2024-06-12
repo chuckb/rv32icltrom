57840eac6cf6760447a09949f2e87213aa690f3f
Chuck Benedict <chuck_benedict@yahoo.com>
2024-06-11 20:45:11 -0700

    Support immediate arithmetic ops

--- buildrom.py
+++ buildrom.py
@@ -8,12 +8,12 @@ import csv
 if __name__ == '__main__':
   words = []
 
-  # Create a 512 list (9 bits) mapping to 5 bits of control data output
+  # Create a 512 list (9 bits) mapping to 9 bits of control data output
   for x in range(512):
     words.append(0)
 
   # Create an iterator to chop off first record of truth table
-  iter = itertools.islice(sys.stdin, 1, None)
+  iter = itertools.islice(sys.stdin, 2, None)
 
   # Open the truth table csv file
   csv_reader = csv.DictReader(iter)
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     line = ""
     # Iterate over each word
     for y in range(16):
-      line += f"{words[(x*16) + y]:0>2x} "
+      line += f"{words[(x*16) + y]:0>3x} "
     # Chop off the last space
     line = line[:-1]
     print(line)
